b1ec8a486d33281d62d39bffa6495936af03e375
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2021-10-29 16:23:26 +0200

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports = (client, int) => {
     if (!int.isButton()) return;
 
@@ -7,8 +9,23 @@ module.exports = (client, int) => {
         case 'saveTrack': {
             if (!queue || !queue.playing) return int.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true, components: [] });
 
-            int.member.send(`You saved the track ${queue.current.title} | ${queue.current.author} from the server ${int.member.guild.name} ✅`).then(() => {
-                return int.reply({ content: `I have sent you the title of the music by private messages ✅`, ephemeral: true, components: [] });
+            int.member.send({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor('RED')
+                        .setTitle(`:arrow_forward: ${queue.current.title}`)
+                        .setURL(queue.current.url)
+                        .addFields(
+                            { name: ':hourglass: Duration:', value: `\`${queue.current.duration}\``, inline: true },
+                            { name: 'Song by:', value: `\`${queue.current.author}\``, inline: true },
+                            { name: 'Views :eyes:', value: `\`${queue.current.views}\``, inline: true },
+                            { name: 'Song URL:', value: `\`${queue.current.url}\`` }
+                        )
+                        .setThumbnail(queue.current.thumbnail)
+                        .setFooter(`from the server ${int.member.guild.name}`, int.member.guild.iconURL({ dynamic: false }))
+                    ]
+            }).then(() => {
+                return int.reply({ content: `I have sent info of the music by private messages ✅`, ephemeral: true, components: [] });
             }).catch(error => {
                 return int.reply({ content: `Unable to send you a private message... try again ? ❌`, ephemeral: true, components: [] });
             });
